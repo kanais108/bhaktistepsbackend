0e70ddb43f88e60a8970da44793f937dc15187fa
Rohit Singh <rohit_singh1@vfc.com>
2026-05-02 18:10:46 +0530

    Fix Body import for notifications

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Post, Req, Body, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { NotificationsService } from './notifications.service';
